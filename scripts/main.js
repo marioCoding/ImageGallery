2b1d0d7e62db573f61fb7bfacd0b56d5995c0a1d
artdm lab <artdm@dvc.edu>
2023-03-09 11:47:45 -0800

    messed with traversing my captions array

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,12 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // define figcaptions for each slide 
-  const captions = [
-    "Blame! (stylized as BLAME!) is a Japanese science fiction manga series written and illustrated by Tsutomu Nihei",
-    "Demon Slayer: Kimetsu no Yaiba 鬼滅の刃, is a Japanese manga series written and illustrated by Koyoharu Gotouge.",
-    "Spy × Family is a Japanese manga series written and illustrated by Tatsuya Endo.",
-    "Dragon Ball Z is the English title for the last two thirds of the Dragon Ball manga, which was written and illustrated by Akira Toriyama.",
-    "One-Punch Man is a Japanese superhero manga series created by One."  
-  ]
-
   //create shortcut vars
   const back_btn = document.querySelector(".back-btn");
   const next_btn = document.querySelector(".next-btn");
   const frame = document.querySelector(".frame");
   const slides = frame.querySelectorAll("img");
-  const caption = document.querySelector(".caption");
+  
 
   //with JS active, hide all images
   slides.forEach((slide) => {
@@ -34,7 +25,17 @@ function changeSlide(e) {
   // stop link from trying to reload page
   e.preventDefault();
   
+  // define figcaptions for each slide 
+  const captions = [
+    "Blame! (stylized as BLAME!) is a Japanese science fiction manga series written and illustrated by Tsutomu Nihei",
+    "Demon Slayer: Kimetsu no Yaiba 鬼滅の刃, is a Japanese manga series written and illustrated by Koyoharu Gotouge.",
+    "Spy × Family is a Japanese manga series written and illustrated by Tatsuya Endo.",
+    "Dragon Ball Z is the English title for the last two thirds of the Dragon Ball manga, which was written and illustrated by Akira Toriyama.",
+    "One-Punch Man is a Japanese superhero manga series created by One."  
+  ]
+
   //shortcut vars
+  const caption = document.querySelector(".caption");
   const frame = document.querySelector(".frame");
   const slides = frame.querySelectorAll("img");
   let showing = document.querySelector(".current");
@@ -66,11 +67,16 @@ function changeSlide(e) {
   nextUp.classList.toggle("current");
   
   // grab the alt text from current image
+  let imgInfo = captions[0];
   let altText = nextUp.getAttribute('alt');
   console.log(altText);
-
+  
   // change the figcaption to the alt text;
-  caption.innerHTML = altText;
+  for (let i = 0; i < captions.length(); i++) {
+    if (nextUp)
+      caption.innerHTML = imgInfo;
+  }
+
 }
 
 // Consider this: a different way to cylce through captions
